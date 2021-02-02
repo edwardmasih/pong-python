584a498ebed0be67cb971147bfcf0da5214236ac
Edward Masih <masih.edward45@gmail.com>
2021-02-02 22:04:25 +0530

    lesson 1 - created the board

---
+++ pong.py
@@ -0,0 +1,42 @@
+# Simple Pong by @edwardmasih
+
+import turtle
+
+# Window
+wn = turtle.Screen()
+wn.title("Pong by @edwardmasih")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+# Paddle A
+paddleA = turtle.Turtle()
+paddleA.speed(0)
+paddleA.shape("square")
+paddleA.color("white")
+paddleA.shapesize(stretch_wid=5, stretch_len=1)
+paddleA.penup()
+paddleA.goto(-350, 0)
+
+# Paddle B
+paddleB = turtle.Turtle()
+paddleB.speed(0)
+paddleB.shape("square")
+paddleB.color("white")
+paddleB.shapesize(stretch_wid=5, stretch_len=1)
+paddleB.penup()
+paddleB.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
+# Main Game Loop
+while True:
+    wn.update()
